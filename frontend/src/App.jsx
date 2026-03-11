32ca2069777cfd495b16052c5fad74bab73ce7d3
Haris Ali <harisalimughal077@gmail.com>
2026-03-11 23:32:18 +0500

    UI Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import AppProduct from './pages/AppProduct'
+import AppLoader from './pages/AppLoader'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/app" element={<AppProduct />} />
+      <Route path="/app" element={<AppLoader />} />
+      <Route path="/app/product" element={<AppProduct />} />
     </Routes>
   )
 }
